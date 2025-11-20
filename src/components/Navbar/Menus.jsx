9196dc006257869e0fcbc54863b768b2aef5ff0e
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-11-20 19:22:47 +0600

    navbar components design

---
+++ src/components/Navbar/Menus.jsx
@@ -0,0 +1,18 @@
+export default function Menus() {
+  return (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>My-Bookings</a>
+      </li>
+      <li>
+        <a>Blogs</a>
+      </li>
+      <li>
+        <a>Contact Us</a>
+      </li>
+    </>
+  );
+}
